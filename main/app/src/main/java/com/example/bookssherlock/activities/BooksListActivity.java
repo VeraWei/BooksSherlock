8afb99e20ecd0a9a460995b0de194048f3749d3d
strogiyotec <almas337519@gmail.com>
2020-10-17 17:16:30 -0700

    Add bottom navigation bar

--- main/app/src/main/java/com/example/bookssherlock/activities/BooksListActivity.java
+++ main/app/src/main/java/com/example/bookssherlock/activities/BooksListActivity.java
@@ -1,22 +1,39 @@
 package com.example.bookssherlock.activities;
 
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.recyclerview.widget.RecyclerView;
-
 import android.os.Bundle;
 
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AppCompatActivity;
+
 import com.example.bookssherlock.R;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class BooksListActivity extends AppCompatActivity {
-    private RecyclerView recyclerView;
-    private BookListAdapter recyclerAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_books_list);
-        recyclerView = findViewById(R.id.recyclerView);
+        final ActionBar toolBar = getSupportActionBar();
 
+        final BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
+        navigation.setOnNavigationItemSelectedListener(item -> {
+            switch (item.getItemId()) {
+                case R.id.navigation_home:
+                    toolBar.setTitle("Home");
+                    return true;
+                case R.id.navigation_cards:
+                    toolBar.setTitle("Cards");
+                    return true;
+                case R.id.navigation_price:
+                    toolBar.setTitle("Books List");
+                    return true;
+                case R.id.navigation_profile:
+                    toolBar.setTitle("My Profile");
+                    return true;
+                default:
+                    return false;
+            }
+        });
     }
-
 }
